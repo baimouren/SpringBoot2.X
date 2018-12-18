cd5cd43f13d7fda18b9df0215d4f45965ba43896
baimouren <18321749330@163.com>
2018-12-18 22:52:13 +0800

    没写好----增删改查user表

--- demo1/src/main/java/com/boot/demo/controller/pg/PgController.java
+++ demo1/src/main/java/com/boot/demo/controller/pg/PgController.java
@@ -1,13 +1,8 @@
-package com.boot.demo.controller;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+package com.boot.demo.controller.pg;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,26 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.boot.demo.service.manage.UserService;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @ComponentScan("com.boot.demo.*")
-//@MapperScan("com.boot.demo.mapper")
+@MapperScan("com.boot.demo.mapper")
 @Controller
 public class PgController {
 	private static final Logger logger = LoggerFactory.getLogger(PgController.class);
 	
-	@Autowired
-	private UserService userService;
-	
-	@RequestMapping(value="/{pg}",method = RequestMethod.GET)
+	@ResponseBody
+	@RequestMapping(value="/pg/{pg}",method = RequestMethod.GET)
 	public String pg(@PathVariable String pg) {
-		
-		Map<String, String> map = new HashMap<>();
-		List<Map<String, Object>> query = userService.select(map);
-		
-		logger.info(query.toString());
+		logger.info("页面名："+ pg + ".jsp");
 		return pg;
 	}
 	
