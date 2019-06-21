377b735c419d3ff11f992dcb359cd02debd7bfa9
baimouren <18321749330@163.com>
2019-06-21 13:24:24 +0800

    security 简单实现

--- look-bilibili/src/main/java/com/example/lookbilibili/controller/PageController.java
+++ look-bilibili/src/main/java/com/example/lookbilibili/controller/PageController.java
@@ -2,6 +2,7 @@ package com.example.lookbilibili.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,12 +14,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
+    @RequestMapping("/p/{pageName}")
+    public String getPage(@PathVariable String pageName){
+            return pageName;
+    }
+
     @RequestMapping("/index")
-    public String getPage(ModelMap map){
-        // 加入一个属性，用来在模板中读取
-        map.addAttribute("host", "loclahost");
-        // return模板文件的名称，对应src/main/resources/templates/index.html
-        return "index";
+    public String getPage(){
+            return "index";
+    }
+
+    /**
+     * 根据配置，Spring Security提供了一个过滤器来拦截请求并验证用户身份。
+     * 如果用户身份认证失败，页面就重定向到/login?error，并且页面中会展现相应的错误信息。
+     * 若用户想要注销登录，可以通过访问/login?logout请求，在完成注销之后，页面展现相应的成功消息。
+     *
+     * 到这里，我们启用应用，并访问http://localhost:8080/，可以正常访问。
+     * 但是访问http://localhost:8080/hello的时候被重定向到了http://localhost:8080/login页面，因为没有登录，用户没有访问权限，
+     * 通过输入用户名user和密码password进行登录后，跳转到了Hello World页面，
+     * 再也通过访问http://localhost:8080/login?logout，就可以完成注销操作。
+     */
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
