a17ef282d18a0362acfc1d1aa727859b6e14c349
baimouren <18321749330@163.com>
2019-07-12 16:55:06 +0800

    线程池测试

--- look-bilibili/src/test/java/com/example/lookbilibili/controller/async/AsyncControllerTest.java
+++ look-bilibili/src/test/java/com/example/lookbilibili/controller/async/AsyncControllerTest.java
@@ -1,26 +1,32 @@
 package com.example.lookbilibili.controller.async;
 
+import javafx.application.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.*;
 
 /**
- * @Description TODO
+ * @Description 测试
  * @Auther CB
  * @Date 2019-07-11 16:34
  * @Version 1.0
  **/
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes  = MockServletContext.class)
 @WebAppConfiguration
 public class AsyncControllerTest {
@@ -35,7 +41,14 @@ public class AsyncControllerTest {
     @Test
     public void asyncPage() throws Exception {
 
-        mvc.perform(MockMvcRequestBuilders.get("/async/page"));
+        mvc.perform(MockMvcRequestBuilders.get("/async/page")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .param("lat", "123.123").param("lon", "456.456")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print()
+        );
 
     }
+
 }
