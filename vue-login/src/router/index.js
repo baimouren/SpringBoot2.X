65a9a3abcfbdafb4b627bedd6089310cf6e6abe9
baimouren <18321749330@163.com>
2019-07-15 13:23:13 +0800

    vue-login

---
+++ vue-login/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: '登录',
+      hidden: true,
+      component: Login
+    }, {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }, {
+      path: '/Login',
+      name: '登录',
+      component: Login
+    }
+  ]
+})
