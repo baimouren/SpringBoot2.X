162d7c8990b2f5ab1b91c7395b677fbc5dc594f1
baimouren <18321749330@163.com>
2019-07-15 14:06:27 +0800

    vue-login 成功案例

--- vue-login/src/router/index.js
+++ vue-login/src/router/index.js
@@ -9,11 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: '登录',
-      hidden: true,
-      component: Login
-    }, {
-      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     }, {
