2b88cc8a4190d4dc4722c2ffa65317055bd2ca08
baimouren <18321749330@163.com>
2019-08-19 15:31:26 +0800

    注释 请求参数转string 不用的代码

--- vue-login/src/utils/api.js
+++ vue-login/src/utils/api.js
@@ -7,14 +7,14 @@ export const postJsonRequest = (url, params) => {
     method: 'post',
     url: `${base}${url}`,
     data: params,
-    transformRequest: [function (data) {
-      // Do whatever you want to transform the data
-      let ret = ''
-      for (let it in data) {
-        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-      }
-      return ret
-    }],
+    // transformRequest: [function (data) {
+    //   // Do whatever you want to transform the data
+    //   let ret = ''
+    //   for (let it in data) {
+    //     ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+    //   }
+    //   return ret
+    // }],
     headers: {
       'Content-Type': 'application/json'
     }
