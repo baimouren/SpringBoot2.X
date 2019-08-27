37f07d3cc6b5a313ac935eb26abc76c4fd85521f
baimouren <18321749330@163.com>
2019-08-27 20:19:53 +0800

    分页

--- vue-login/src/components/Bill.vue
+++ vue-login/src/components/Bill.vue
@@ -41,6 +41,11 @@
       </template>
     </el-table-column>
   </el-table>
+    <el-button-group>
+      <el-button type="primary" icon="el-icon-arrow-left">上一页</el-button>
+      <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+      <span>x/y页</span>
+    </el-button-group>
   </div>
 </template>
 
